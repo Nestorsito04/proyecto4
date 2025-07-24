fff3915e2e9e6b403472fe5ad669476916b0df5e
Nestorsito04 <nestorrafaelbot@gmail.com>
2025-07-24 14:28:15 -0400

    Update m.hpp

--- m.hpp
+++ m.hpp
@@ -15,4 +15,35 @@ namespace MatrizLib {
         void liberarMemoria();
         double calcularDeterminante(double** m, int n) const;
 
+public:
+        Matriz();
+        Matriz(int f, int c);
+        Matriz(const Matriz& otra);
+        Matriz& operator=(const Matriz& otra);
+        ~Matriz();
+
+        void llenarMatriz();
+        void mostrarMatriz() const;
+        Matriz transpuesta() const;
+        double determinante() const;
+        Matriz suma(const Matriz& otra) const;
+        Matriz multiplicacion(const Matriz& otra) const;
+        bool esIgual(const Matriz& otra) const;
+
+        void setValor(int f, int c, double valor);
+        double getValor(int f, int c) const;
+        int getFilas() const;
+        int getColumnas() const;
+
+        void anadirFila();
+        void anadirColumna();
+    };
+
+    Matriz crearMatriz(int filas, int columnas);
+
+}
+
+#endif
+
+
 
